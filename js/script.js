29766c91d4db8e3c31dd0007cd8800e89300c5e5
Umang <umang26112003@gmail.com>
2022-05-26 20:06:04 +0530

    a

--- js/script.js
+++ js/script.js
@@ -17,12 +17,10 @@ var personal_body = (
 	+ "-> web design(judge me yourself lol)" + br
 	+ "-> mathematics" + br
 	+ "-> and linguistics(failed to qualify the olympiad three years in a row)" + br
-
 	+ hr
-	+ "Some aliases I use on the internet (the public ones atleast 😉 ):" + br
-	+ "-> umang1126" + br 
-	+ "-> ZeroDawn0D or ZeroDawn.0D (depending on availability)" + br
-	+ "-> the_oblivious_psyduck"
+	+ "Some interesting facts about me:" + br
+	+ "-> My favourite font is Ubuntu" + br 
+	+ "-> I do not have a second favourite font" + br
 	+ hr);
 
 var personal = document.getElementById("personal");
@@ -38,7 +36,7 @@ var sch_body = (
 	hr
 	+ "-> In 11th Grade I qualified for the KVPY Scholarship to pursue my undergraduate research studies in India" + br
 	+ "-> \"The <b>Kishore Vaigyanik Protsahan Yojana (KVPY)</b> is an on-going National Program of Fellowship in Basic Sciences, initiated and funded by the <b>Department of Science and Technology, Government of India</b>, to attract exceptionally highly motivated students for pursuing basic science courses and research career in science.\"" + br
-	+ "-> <a href = \"http://www.kvpy.iisc.ernet.in/main/index.htm\">KVPY Website</a>"
+	+ "-> <a id = \"kvpy_link\" href = \"http://www.kvpy.iisc.ernet.in/main/index.htm\">KVPY Website</a>"
 	+ hr
 	+ "-> In 10th Grade I qualified the first stage of the <b>National Talent Search Examination</b>, then a small impediment happened in early 2020 by the name of COVID-19 and in the process (which involved date clashes with other examinations) I never appeared for the second stage :("
 	+hr
